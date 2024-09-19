85c800d36927e71a64efde5bccc36e68dbc27326
Hameed-usman <usman.hameed1145@gmail.com>
2024-09-19 14:31:26 +0500

    practise 07

---
+++ practise-05/JS Practical 07/seven.js
@@ -0,0 +1,43 @@
+// alert("Sveen......folder.!!!");
+
+// Question  01
+
+// let inpt = prompt("Enter a string:");
+// let index = +prompt("Enter an index:");
+
+// let char = inpt.charAt(index);
+// alert("Character at index " + index + ": " + char);
+
+
+// QUESTION 02 
+
+// let str = prompt("Enter a string:");
+// let searchstr = prompt("Enter a substring to search for:");
+// let replacestr = prompt("Enter a replacement substring:");
+
+// let modifiedStr = str.replace(searchstr, replacestr);
+// alert("Modified string: " + modifiedStr);
+
+
+// QUESTION 03 
+
+// let num = +prompt("Enter a number:");
+
+// let roundedNum = Math.round(num);
+// alert("Rounded number: " + roundedNum);
+
+
+// QUESTION  04
+
+// let num = +prompt("Enter a number:");
+
+// let ceilNum = Math.ceil(num);
+// console.log("Ceiling of the number: " + ceilNum);
+
+
+// QUESTION 05 
+
+// let num = +prompt("Enter a number:");
+
+// let floorNum = Math.floor(num);
+// alert("Floor of the number: " + floorNum);
