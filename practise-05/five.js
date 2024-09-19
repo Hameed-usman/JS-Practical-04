219be5deb9ded346975cf6618ff845ea50361603
Hameed-usman <usman.hameed1145@gmail.com>
2024-09-19 14:17:18 +0500

    Practise05

---
+++ practise-05/five.js
@@ -0,0 +1,63 @@
+// console.log("Runnning.!??");
+
+// ASSIGNMENT 01 
+
+// let arr = [1, 2, 3, 4, 5];
+
+
+// arr.shift();
+// arr.shift(); 
+
+// arr.push(6, 7, 8);
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+
+                     // ASSIGNMENT 02 
+
+// let alpha = [1, 2, 3, 4, 5];
+
+// for (let i = 0; i < alpha.length; i++) {
+//   if (alpha[i] % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+
+          // ASSIGNMENT 03 
+
+// let beta = [3, 4, 5];
+
+// beta.unshift(1, 2);
+
+// beta.pop();
+
+// for (let i = 0; i < beta.length; i++) {
+//   console.log(beta[i]);
+// }
+
+
+               // ASSIGNMENT 04
+
+// let delta = [1, 2, 3, 4, 5];
+
+// for (let i = 1; i < delta.length; i++) {
+//   if (delta[i] > delta[i - 1]) {
+//     console.log(delta[i]);
+//   }
+// }
+
+
+// ASSIGNMENT 05
+
+// let aray = [1, 2, 3, 4, 5];
+
+// let index = aray.indexOf(3);
+
+// aray.splice(index);
+
+// for (let i = 0; i < aray.length; i++) {
+//   console.log(aray[i]);
+// }
